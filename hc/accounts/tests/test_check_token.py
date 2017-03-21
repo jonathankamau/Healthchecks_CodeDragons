75701b24f465514611a671abcb5465eecd466663
jonathankamau <jonathan.kamau@andela.com>
2017-03-21 11:52:39 +0300

    added one test

--- hc/accounts/tests/test_check_token.py
+++ hc/accounts/tests/test_check_token.py
@@ -15,6 +15,8 @@ class CheckTokenTestCase(BaseTestCase):
 
     def test_it_redirects(self):
         r = self.client.post("/accounts/check_token/alice/secret-token/")
+        self.assertEqual(r.status_code, 302)
+        # self.assertIn(self.profile.user, self.client.session)
         self.assertRedirects(r, "/checks/")
 
         # After login, token should be blank
@@ -24,5 +26,8 @@ class CheckTokenTestCase(BaseTestCase):
     ### Login and test it redirects already logged in
 
     ### Login with a bad token and check that it redirects
+    def test_redirects_login_with_bad_token(self):
+        r = self.client.post("/accounts/check_token/alice/bad-token/")
+        self.assertRedirects(r,"/accounts/login/")
 
     ### Any other tests?

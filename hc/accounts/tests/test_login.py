9129c195465e3959267fff3ae875d45fb4918fc8
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-03-24 12:29:30 +0300

    Modified two tests

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -21,17 +21,22 @@ class LoginTestCase(TestCase):
         assert r.status_code == 302
 
         ### Assert that a user was created
-         
-        self.assertIn("alice@example.org", session)
+
+        user_check = User.objects.filter(email="alice@example.org")
+        self.assertEqual(user_check[0].email, form["email"])
+
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
 
         ### Assert contents of the email body
-        self.assertIn('Hello \n To log into healthchecks.io, please open the link below:', mail.outbox[0].body)
+        self.assertIn('Hello,\n\nTo log into healthchecks.io, please open the link below:', mail.outbox[0].body)
 
         ### Assert that check is associated with the new user
-        self.assertIn('alice@example.org', session)
+        user = User.objects.get(email="alice@example.org")
+
+        test_user = Check.objects.get(user=user)
+        self.assertEqual(test_user.user, user)
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
